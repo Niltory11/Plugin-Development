f6862fb9821770f7b25e8a292ebdab105e43928d
Rafat <ahsanhabibrafat11@gmail.com>
2025-08-04 13:41:56 +0600

    metadata added for this plugin

--- simple-scroll-to-top-wp.php
+++ simple-scroll-to-top-wp.php
@@ -1,4 +1,20 @@
 <?php
+/**
+ * plugin Name:     Simple Scroll to Top WP
+ * Plugin URI:      https://wordpress.org/plugins/simple-scroll-to-top-wp/
+ * description:     A simple plugin to add a scroll to top button on your WordPress site.
+ * version:         1.0.0 
+ * requires at least: 5.0
+ * requires PHP:    7.2
+ * author:          Ahsan Habib Rafat
+ * author URI:      https://github.com/Niltory11
+ * license:         GPLv2 or later
+ * license URI:     https://www.gnu.org/licenses/gpl-2.0.html
+ * update URI:      https://github.com/Niltory11
+ * text domain:     sstwp-rafat
+ * domain path:     /languages   
+ */
+
 
 
 ?>
